5ebd8cdf7cf765c5a2a426b9011b4e0d455e1031
yarik <yariks@i.ua>
2019-10-19 09:26:57 +0300

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 }
